fd6ee90190c972588c45759abb980077372066be
benjamin <benjamin.wu@leandev.se>
2016-02-24 21:10:16 +0800

    增加了安全properties配置

---
+++ src/main/java/com/benjamin/common/security/EncrytPropertyPlaceHolder.java
@@ -0,0 +1,42 @@
+package com.benjamin.common.security;
+
+import net.iharder.Base64;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import java.io.IOException;
+
+/**
+ * Created by piqiu on 2/24/16.
+ * 安全解密配置信息
+ */
+public class EncrytPropertyPlaceHolder extends PropertyPlaceholderConfigurer {
+
+    private final String[] encryptPropNames = {"hibernate.connection.username", "hibernate.connection.password",
+            "jdbc.username", "jdbc.password"};
+
+    @Override
+    protected String convertProperty(String propertyName, String propertyValue) {
+        if (isContainsProp(propertyName)) {
+            String descryptValue = null;
+            try {
+                descryptValue = (String) Base64.decodeToObject(propertyValue);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return descryptValue;
+        } else {
+            return propertyValue;
+        }
+    }
+
+    private boolean isContainsProp(String propertyName) {
+        for (String prop: encryptPropNames) {
+            if (prop.equals(propertyName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
